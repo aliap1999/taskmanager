afdd477002a2a679cba510d9989207f40f2b4d56
ali <samurai.irann@gmail.com>
2025-11-21 16:20:42 +0330

    Add user authentication (signup and login)

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login'), 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
 from .models import Task
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'tasks/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid(): 
+            user = form.get_user()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'tasks/login.html', {'form': form})
 
 def task_list(request):
     tasks = Task.objects.all()
